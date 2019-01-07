d3567323225f82f5cc138d0cf148ebcceff1bcfc
Sarab <sarab.sethi93@gmail.com>
2019-01-07 08:52:06 +0000

    reverse wifi reset switch to 0 is active

--- libs/reset_device/reset.py
+++ libs/reset_device/reset.py
@@ -5,7 +5,7 @@ import subprocess
 import reset_lib
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 counter = 0
 serial_last_four = subprocess.check_output(['cat', '/proc/cpuinfo'])[-5:-1].decode('utf-8')
@@ -21,7 +21,7 @@ if hostapd_reset_required == True:
 # This is the main logic loop waiting for a button to be pressed on GPIO 20 for 10 seconds.
 # If that happens the device will reset to its AP Host mode allowing for reconfiguration on a new network.
 while True:
-    while GPIO.input(20) == 1:
+    while GPIO.input(20) == 0:
         time.sleep(1)
         counter = counter + 1
 
@@ -30,7 +30,7 @@ while True:
         if counter == 9:
             reset_lib.reset_to_host_mode()
 
-        if GPIO.input(20) == 0:
+        if GPIO.input(20) == 1:
             counter = 0
             break
 
